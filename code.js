546780e56c4f0112d9b3fa858862549dff3bba5a
morganjwilson <64968479+morganjwilson@users.noreply.github.com>
2020-07-29 10:24:14 -0400

    Complete

--- code.js
+++ code.js
@@ -5,7 +5,7 @@
 */
 
 /*
-Exercise One - Accessing an array
+Exercise One - Accessing an array X
 
 */
 
@@ -15,15 +15,15 @@ let sentence = ["The", "dog", "jumped", "over", "the", "fence"];
 // Log the word "jumped" to the console by accessing it in the array.
 
 // Your Code Here
-
+console.log(sentence[2]);
 // Step Two ------------------
 // Log the last word in the array to the console.
 
 // Your Code Here
-
+console.log(sentence[5]);
 /*
 ---------------------------------------------------------------------------
-Exercise Two - Adding up numbers
+X Exercise Two - Adding up numbers
 
 Create a statement which adds up every number of the array, assigning 
 the sum to a new variable.
@@ -34,10 +34,10 @@ Then log the sum to the console. (Hint: It should be 28)
 let numbers = [3, 4, 5, 7, 9];
 
 // Your Code Here
-
+let sum = "3" + "4" + "5" + "7" + "9";
 /*
 ---------------------------------------------------------------------------
-Exercise Three - Creating an array
+X Exercise Three - Creating an array
 
 */
 
@@ -46,13 +46,14 @@ Exercise Three - Creating an array
 // The array should be assigned to a variable named "colors"
 
 // Your Code Here
-
+let colors = ["red", "orange", "yellow", "green", "purple"];
+console.log(colors.length);
 // Step Two ------------------
 // Log the length of the array to the console (it should print 5)
-
+console.log(colors.length);
 /*
 ---------------------------------------------------------------------------
-Exercise Four - Updating an array
+X Exercise Four - Updating an array 
 
 */
 
@@ -63,13 +64,19 @@ let names = []; // Don't modify this line.
 // Then log your array to the console.
 
 // Your Code Here
-
+names[1] = "morgan";
+names[2] = "brandon";
+names[3] = "bre";
+names[4] = "john";
+names[5] = "sally";
+console.log(names);
 // Step Two ------------------
 // Using the index variable, assign a new name to the given index.
 // Then log the modified array to the console.
 let index = 2;
-
 // Your Code Here
+names.push("hannah");
+console.log(names);
 
 /*
 ---------------------------------------------------------------------------
@@ -82,7 +89,7 @@ You will need to assign the result to a new variable.
 Then log the new array to the console.   Hint: Use concat, this was shown in the reading.
 */
 
+// Your Code Here.
 let firstArray = ["This", "will", "make"];
 let secondArray = ["a", "combined", "array."];
-
-// Your Code Here.
+let combinedArray = firstArray.concat(secondArray);
